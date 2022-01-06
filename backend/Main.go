100abe61737c6fa921e1e180a15962ab4099d144
kennethk-1201 <63090851+kennethk-1201@users.noreply.github.com>
2022-01-06 22:46:15 +0800

    create endpoints

--- backend/Main.go
+++ backend/Main.go
@@ -1,14 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // "encoding/json"
-// "log"
-// "net/http"
 // "math/rand"
 // "strconv"
-// "github.com/gorilla/mux"
 
 func main() {
-	fmt.Println("first print!")
+	// Init Router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/task", getTasks).Methods("GET")
+	r.HandleFunc("/api/task/{id}", getTask).Methods("GET")
+	r.HandleFunc("/api/task/add", createTask).Methods("POST")
+	r.HandleFunc("/api/task/{id}", updateTask).Methods("PUT")
+	r.HandleFunc("/api/task/{id}", deleteTask).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getTasks() {
+
+}
+
+func getTask() {
+
+}
+
+func createTask() {
+
+}
+
+func updateTask() {
+
+}
+
+func deleteTask() {
+
 }
