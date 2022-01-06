8e97e0520fb6898c47cea6023d202d8985d76244
kennethk-1201 <63090851+kennethk-1201@users.noreply.github.com>
2022-01-06 22:40:02 +0800

    add dependency declaration and gitignore files

---
+++ backend/Main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// "encoding/json"
+// "log"
+// "net/http"
+// "math/rand"
+// "strconv"
+// "github.com/gorilla/mux"
+
+func main() {
+	fmt.Println("first print!")
+}
