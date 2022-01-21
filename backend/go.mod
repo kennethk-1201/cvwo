f8cb39368d0180c596395740ff38068e5abc66bf
kennethk-1201 <63090851+kennethk-1201@users.noreply.github.com>
2022-01-22 03:18:14 +0800

    remove viper

--- backend/go.mod
+++ backend/go.mod
@@ -28,7 +28,6 @@ require (
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.10.1 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
--- backend/internal/helper/helpers.go
+++ backend/internal/helper/helpers.go
@@ -4,48 +4,15 @@ import (
 	"log"
 	"database/sql"
 	"os"
-	"github.com/spf13/viper"
 )
 
 func CheckErr(err error) {
 	if err != nil {
         log.Fatalf("Error occured: %q", err)
 	}
-} 
-
-// use viper package to read .env file
-// return the value of the key
-func GetEnvVariable(key string) string {
-
-	// SetConfigFile explicitly defines the path, name and extension of the config file.
-	// Viper will use this and not check any of the config paths.
-	// .env - It will search for the .env file in the current directory
-	viper.SetConfigFile(".env")
-
-	// Find and read the config file
-	err := viper.ReadInConfig()
-
-	if err != nil {
-		log.Fatalf("Error while reading config file %s", err)
-	}
-
-	// viper.Get() returns an empty interface{}
-	// to get the underlying type of the key,
-	// we have to do the type assertion, we know the underlying value is string
-	// if we type assert to other type it will throw an error
-	value, ok := viper.Get(key).(string)
-
-	// If the type is a string then ok will be true
-	// ok will make sure the program not break
-	if !ok {
-		log.Fatalf("Invalid type assertion")
-	}
-
-	return value
 }
 
 func SetupDB() *sql.DB {
-	os.Setenv("DATABASE_URL", GetEnvVariable("DATABASE_URL")) // remove in production
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
 	CheckErr(err)
